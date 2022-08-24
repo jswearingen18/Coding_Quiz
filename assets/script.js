030db4d6c01be06c3ec33d3fc0aae465670d41d6
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-08-24 16:20:48 -0500

    WIP added more questions and changed JS to try and improve functionality

--- assets/script.js
+++ assets/script.js
@@ -3,32 +3,26 @@ var timerEl = document.getElementById("timer");
 var beginTimer = document.getElementById("beginQ");
 var quizHeader = document.getElementById("quizIntro");
 var quizRules = document.querySelector("#quiz");
-var quizQues = document.getElementById("quizQuestions")
-var answers = document.getElementById("quizAnswer");
+var quizQues = document.getElementById("quizQuestions");
+// variable to create click event for all answer buttons
+const ansTarget = document.querySelectorAll(".ans-btn")
+// buttons for selecting an answer
 let answersA = document.getElementById("answerA");
-var answersB = document.getElementById("answerB");
-var answersC = document.getElementById("answerC");
-var answersD = document.getElementById("answerD");
-// Arrays to be used for questions
-var quizQuestions = ["What does CSS stand for?","What does URL stand for?", "Which of these are a primitive Data type?"] 
-var quizAwnserA = ["A. Color Selector Sheet", "A. Ultimate Response Listening", "A. JSON"]
-var quizAwnserB = ["B. Cascading Style Sheet", "B. Unlimited Review Logistics", "B. Array"]
-var quizAwnserC = ["C. Colaborating Styling Sheet", "C. Uniform Resource Locator", "C. Class"]
-var quizAwnserD = ["D. Computer Styling Sheet", "D. Ur Response Logistics", "D. String"]
-// Click Functions
+let answersB = document.getElementById("answerB");
+let answersC = document.getElementById("answerC");
+let answersD = document.getElementById("answerD");
+// Click Function to begin timer and quiz
 beginTimer.addEventListener("click", e => {
     countdown();
+    // clears quizHeader area for quiz 
     quizHeader.textContent = " ";
+    // clears rules out to display quiz
     quizRules.textContent = " ";
-    quizQues.textContent = quizQuestions[0];
-    answersA.textContent = quizAwnserA[0];
-    answersB.textContent = quizAwnserB[0];
-    answersC.textContent = quizAwnserC[0];
-    answersD.textContent = quizAwnserD[0];
+   question1 ();
 });
-
+// Function for timer
 function countdown() {
-    var timeLeft = 20;
+    var timeLeft = 100;
     var timeInterval = setInterval(function () {
         if (timeLeft > 1) {
             timerEl.textContent = timeLeft + " seconds remaining";
@@ -42,22 +36,101 @@ function countdown() {
         }
     }, 1000);
 }
-function beginQuiz () {
-    if(beginTimer.clicked == true) {
-        quiz.textContent = " ";
+function question1 () {
+    answersA.addEventListener("click", question1);
+    answersB.addEventListener("click", question1);
+    answersC.addEventListener("click", question1);
+    answersD.addEventListener("click", question1);
+    const ques1 = "What does CSS stand for?";
+    const ans1 = "B. Cascading Style Sheet";
+    const opt1 = ["A. Color Selector Sheet", "B. Cascading Style Sheet", "C. Colaborating Styling Sheet", "D. Computer Styling Sheet"]
+    quizQues.textContent = ques1;
+    answersA.textContent = opt1[0];
+    answersB.textContent = opt1[1];
+    answersC.textContent = opt1[2];
+    answersD.textContent = opt1[3];
+    if (question1.target === ans1) {
+        question2 ();
+    } else {
+        timeLeft -= 10;
     }
 }
-function actualQuiz() {
-    quiz.textContent = quizQuestions[0];
-    answersA.textContent = quizAwnserA[0];
-    answersB.textContent = quizAwnserB[0];
-    answersC.textContent = quizAwnserC[0];
-    answersD.textContent = quizAwnserD[0];
-    if(answersB) {}
-        quiz.textContent = quizQuestions[1];
-        answersA.textContent = quizAwnserA[1];
-        answersB.textContent = quizAwnserB[1];
-        answersC.textContent = quizAwnserC[1];
-        answersD.textContent = quizAwnserD[1];
+function question2 () {
+    answersA.addEventListener("click", question2);
+    answersB.addEventListener("click", question2);
+    answersC.addEventListener("click", question2);
+    answersD.addEventListener("click", question2);
+    const ques2 = "What does URL stand for?";
+    const ans2 = "C. Uniform Resource Locator";
+    const opt2 = ["A. Ultimate Response Listening", "B. Unlimited Review Logistics", "C. Uniform Resource Locator", "D. Ur Response Logistics"]
+    quizQues.textContent = ques2;
+    answersA.textContent = opt2[0];
+    answersB.textContent = opt2[1];
+    answersC.textContent = opt2[2];
+    answersD.textContent = opt2[3];
+    if (question2.target === ans2) {
+       question3 ();
+    } else {
+        timeLeft -= 10;
+    }
 }
+function question3 () {
+    answersA.addEventListener("click", question3);
+    answersB.addEventListener("click", question3);
+    answersC.addEventListener("click", question3);
+    answersD.addEventListener("click", question3);
+    const ques3 = "Which of these are a primitive Data type?";
+    const ans3 = "D. String";
+    const opt3 = ["A. JSON", "B. Array", "C. Class", "D. String"];
+    quizQues.textContent = ques3;
+    answersA.textContent = opt3[0];
+    answersB.textContent = opt3[1];
+    answersC.textContent = opt3[2];
+    answersD.textContent = opt3[3];
+    if (question3.target === ans3) {
+        question4 ();
+    } else {
+        timeLeft -= 10;
+    }
+}    
+function question4 () {
+    answersA.addEventListener("click", question4);
+    answersB.addEventListener("click", question4);
+    answersC.addEventListener("click", question4);
+    answersD.addEventListener("click", question4);
+    const ques4 = "Which of these are a legacy computer language?";
+    const ans4 = "A. JQUERY";
+    const opt4 = ["A. JQUERY", "B. BootStarp", "C. Java", "D. C++"];
+    quizQues.textContent = ques4;
+    answersA.textContent = opt4[0];
+    answersB.textContent = opt4[1];
+    answersC.textContent = opt4[2];
+    answersD.textContent = opt4[3];
+    if (question4.target === ans4) {
+        question5 ();
+    } else {
+        timeLeft -= 10;
+    }
+}   
+function question5 () {
+    answersA.addEventListener("click", question5);
+    answersB.addEventListener("click", question5);
+    answersC.addEventListener("click", question5);
+    answersD.addEventListener("click", question5);
+    const ques5 = "Which of these are used to display coding projects?";
+    const ans5 = "D. Github";
+    const opt5 = ["A. Twitter", "B. Facebook", "C. Indeed", "D. Github"];
+    quizQues.textContent = ques5;
+    answersA.textContent = opt5[0];
+    answersB.textContent = opt5[1];
+    answersC.textContent = opt5[2];
+    answersD.textContent = opt5[3];
+    if (question5.target === ans5) {
+        quizQues.textContent = "Congratulations";
+    } else {
+        timeLeft -= 10;
+    }
+}    
+
+
 
